924215d0d9620e16f1fe187c4adae8adecd87a27
Khailas R <khailas303@gmail.com>
2021-09-02 19:32:50 +0530

    Updated

--- Web Automation/Using OOPS.py
+++ Web Automation/Using OOPS.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
+
 
 URL = "https://www.seleniumeasy.com/test/basic-first-form-demo.html"
 
@@ -15,23 +15,29 @@ class Main(webdriver.Chrome):
         self.teardown = teardown
         os.environ["PATH"] += self.path
         super(Main, self).__init__()
-        print("one")
 
     def first_page(self):
         self.get(URL)
         self.implicitly_wait(3)
-        print("twooo")
+        print("Site opened")
 
-    # def message(self):
+    def popup_skip(self):
+        try:
+            button = self.find_element_by_class_name("at-cm-no-button")
+            button.click()
+            print("Popup skipped")
+        except:
+            print("\n No element with this class name. Skipping the process")
 
-    #     id = self.find_element_by_link_text("user-message")
-    #     # id.send_keys("Hello World")
+    def message(self):
+        msg = self.find_element_by_id("user-message")
+        msg.send_keys("Hello World")
 
-    #     try:
-    #         button = self.find_element_by_class_name("btn btn-default")
-    #         button.click()
-    #     except:
-    #         print("\n No element with this class name. Skipping the process")
+    def text_btn(self):
+        btn = self.find_element_by_css_selector(
+            "button[onclick='showInput();']")
+        btn.click()
+        print("text button clicked")
 
     def sum(self):
         sum1 = self.find_element_by_id("sum1")
@@ -40,26 +46,24 @@ class Main(webdriver.Chrome):
         sum1.send_keys(Keys.NUMPAD1, Keys.NUMPAD4)
         sum2.send_keys(Keys.NUMPAD6, Keys.NUMPAD8)
 
-        try:
-            button = self.find_element_by_class_name("at-cm-no-button")
-            button.click()
-        except:
-            print("\n No element with this class name. Skipping the process")
-
-    def button(self):
+    def sum_button(self):
         btn = self.find_element_by_css_selector(
             "button[onclick='return total()']")
         btn.click()
-        print("two")
+        print("Sum button clicked")
 
     def final(self):
-        WebDriverWait(self, 33).until(
-            EC.text_to_be_present_in_element(
-                (By.CLASS_NAME, "progress-label"),   # Element filteration
-                'Complete!'     # The expected text
+        try:   # Explicit Wait is code you define to wait for a certain condition to occur before proceeding further in the code
+            WebDriverWait(self, 10).until(
+                EC.text_to_be_present_in_element(
+                    (By.CLASS_NAME, "progress-label"),   # Element filteration
+                    'Complete!'     # The expected text
+                )
             )
-        )
-        print("Four")
+            print("Four")
+        except:
+            AttributeError
+            print("\nPassed the AttributeError Succesfully")
 
     def __exit__(self, exc_type, exc_val, exc_to):
         if self.teardown:
@@ -67,10 +71,13 @@ class Main(webdriver.Chrome):
             print("Exit")
 
 
-with Main() as mn:
-    mn.first_page()
-    mn.message()
-    mn.sum()
-    mn.button()
-    mn.final()
-    print("Done")
+if __name__ == "__main__":
+    with Main() as mn:
+        mn.first_page()
+        mn.popup_skip()
+        mn.message()
+        mn.text_btn()
+        mn.sum()
+        mn.sum_button()
+        mn.final()
+        print("Done")
