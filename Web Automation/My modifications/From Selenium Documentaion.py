d6efffd2feacd4cf9e39714e3d3d72916411a2dd
Khailas R <khailas303@gmail.com>
2021-09-02 20:00:27 +0530

    Update

---
+++ Web Automation/My modifications/From Selenium Documentaion.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+driver = webdriver.Chrome()
+driver.get("https://www.python.org")
+assert "Python" in driver.title
+element = driver.find_element_by_name("q")
+element.clear()
+element.send_keys("pycon")
+element.send_keys(Keys.RETURN)
+assert "No results found" not in driver.page_source
+driver.close()
